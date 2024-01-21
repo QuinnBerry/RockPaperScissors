1d2db1a5e21dfe77c01dd6f46ab7f70ad3245309
QuinnBerry <qberry97@outlook.com>
2024-01-20 22:07:04 -0700

    Update game result messages

--- src/game.py
+++ src/game.py
@@ -9,7 +9,19 @@ ROCK = "rock"
 PAPER = "paper"
 SCISSORS = "scissors"
 
+USER_WINNER_MESSAGE = "You Win!"
+CPU_WINNER_MESSAGE = "CPU Wins..."
+TIE_MESSAGE = "It's a Tie!"
+INVALID_INPUT_MESSAGE = "You entered an invalid move"
+
 choices = [ROCK, PAPER, SCISSORS]
+
+winnerMessages = {
+    TIE: TIE_MESSAGE,
+    PLAYER1: USER_WINNER_MESSAGE,
+    PLAYER2: CPU_WINNER_MESSAGE,
+    INVALID_INPUT: INVALID_INPUT_MESSAGE
+}
 
 
 
@@ -41,4 +53,4 @@ if __name__ == '__main__':
     result = playRound(user_choice, cpu_choice)
     print("You chose: {user_choice}, the CPU chose: {cpu_choice}.".format(
         user_choice = user_choice.capitalize(), cpu_choice = cpu_choice.capitalize()))
-    print("{result} wins!".format(result = result.capitalize()))
+    print(winnerMessages[result])
